13125e3118a927e371b5856806e5a4363d80428e
Cloud User <ec2-user@ip-10-18-40-94.em-tcc-test.int>
2018-06-28 06:51:52 +0000

    fix try

--- psql_backup.py
+++ psql_backup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import boto3
 import requests
 import re
@@ -15,6 +14,8 @@ import string
 #from psql import *
 from datetime import datetime
 import logging as log
+
+
 log.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                 level=log.INFO,
                 datefmt='%m/%d/%Y %H:%M:%S')
@@ -23,39 +24,44 @@ log.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
 def create_snapshot(ec2, volumes_ids):
   tstamp = datetime.now().strftime("%Y%m%d_%H%M")
   for v in volumes_ids:
-    volume = ec2.Volume(v)
+    try:
+      volume = ec2.Volume(v)
+    except Exception as e:
+      log.error("%s" % e)
+      sys.exit(1) 
     name = None
     for tag in volume.tags:
       if tag['Key'] == 'Name':
         name = str(tag['Value'])
-    print(name)
     snapshot_value = "%s_%s" % (name,tstamp)
-    print snapshot_value
-    ec2.create_snapshot(
-      VolumeId = v,
-      Description = name.upper() ,
-      TagSpecifications = [
-        {
-          'ResourceType':  'snapshot' ,
-          'Tags': [
-            {
-              'Key': 'Name',
-              'Value': snapshot_value.upper()
-            },
-            {
-              'Key': 'Type',
-              'Value': "ha_backup"
-            },
-            {
-              'Key': 'Volume-id',
-              'Value': v
-            },
-          ]
-        },
-      ]
-    )
-
-
+    try:
+      response = ec2.create_snapshot(
+        VolumeId = v,
+        Description = name.upper() ,
+        TagSpecifications = [
+          {
+            'ResourceType':  'snapshot' ,
+            'Tags': [
+              {
+                'Key': 'Name',
+                'Value': snapshot_value.upper()
+              },
+              {
+                'Key': 'Type',
+                'Value': "ha_backup"
+              },
+              {
+                'Key': 'Volume-id',
+                'Value': v
+              },
+            ]
+          },
+        ]
+      )
+      log.info("Create snapshot %s" % response)
+    except Exception as e:
+      log.error("%s" % e)
+      sys.exit(1) 
 
 def find_psql_volumes(ec2, instance):
   psql_volumes_ids = []
@@ -66,38 +72,33 @@ def find_psql_volumes(ec2, instance):
   for v in instance.block_device_mappings:
     if v['DeviceName']==instance.root_device_name:
       continue
-    volume = ec2.Volume(v['Ebs']['VolumeId'])
+    try:
+      volume = ec2.Volume(v['Ebs']['VolumeId'])
+    except Exception as e:
+      log.error("%s" % e)
+      sys.exit(1) 
     volume_name = None
     volume_complete_name = None
     for tag in volume.tags:
       if tag['Key'] == 'Name':
         volume_complete_name = str(tag['Value'])
         volume_name=str(volume_complete_name.split("_")[0])   
-    print volume_name
-    print volume_complete_name
     pattern = "psql::%s" % instancename
     if pattern.lower() == volume_name.lower():
       psql_volumes_ids.append(volume.id)
   return psql_volumes_ids
 
 def find_all_volume_snapshots(volume_id):
-  client=boto3.client('ec2')
- 
-  r = client.describe_snapshots(Filters= [{'Name': 'tag:Type', 'Values': ['ha_backup']},{'Name': 'tag:Volume-id', 'Values': [volume_id]}])     
-  return r
-#  snapshots = []
-#  volume = ec2.Volume(volume_id)
-#  list_of_snapshots = volume.snapshots.all()
-#  for l in list_of_snapshots:
-#    for tag in l.tags:
-#      if tag["Key"] == 'Type':
-#        if tag['Value'] == "ha_backup":
-#          snapshots.append(l)
-#  return snapshots
+  try: 
+    client=boto3.client('ec2')
+    r = client.describe_snapshots(Filters= [{'Name': 'tag:Type', 'Values': ['ha_backup']},{'Name': 'tag:Volume-id', 'Values': [volume_id]}])     
+    return r
+  except Exception as e:
+    log.error("%s" % e) 
+    sys.exit(1)
 
 def order_snapshots(snapshots):
   return sorted(snapshots, key=lambda k: k['StartTime'], reverse=True)
-
 
 def snapshots_to_remove(volumes_ids,retention):
   snapshots_to_remove = []
@@ -111,13 +112,20 @@ def snapshots_to_remove(volumes_ids,retention):
   return snapshots_to_remove
 
 def delete_old_snaphots(ec2,snaphots):
-
   for s_volume in snaphots:
     for s in s_volume:
-      snapshot = ec2.Snapshot(s["SnapshotId"])
-      response =snapshot.delete()
-      print response
-     
+      try:
+        snapshot = ec2.Snapshot(s["SnapshotId"])
+        volume_id = None
+        for tag in snapshot.tags:
+          if tag['Key'] == 'Volume-id':
+            volume_id = str(tag['Value'])
+        response =snapshot.delete()
+        log.info("\nVolume: %s \nSnapshot: %s \nDelete: %s" % (volume_id,snapshot,response))
+      except Exception as e:
+        log.error("Volume snapshot delete error %s" % (e))
+
+
 def main():
   #if len(args) != 6:
   #  print("db_host,db_name,db_user,db_password,methenv,retention")
@@ -140,7 +148,7 @@ def main():
     ec2 = session.resource('ec2')
     instance = ec2.Instance(inst_id)
     psql_volumes_ids = find_psql_volumes(ec2, instance)
-    print(psql_volumes_ids)
+    log.info("Volume ids to backup: %s" % psql_volumes_ids)
     create_snapshot(ec2,psql_volumes_ids)
     s = snapshots_to_remove(psql_volumes_ids,retention)
     delete_old_snaphots(ec2,s)
